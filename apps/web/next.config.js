2e37ca61d18ac35ea8ec85bf364794ffa94daf15
Kirill Tovpinets <kirill.tovpinets@icloud.com>
2023-11-07 12:45:08 +0100

    move web to app dir

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const NextFederationPlugin = require('@module-federation/nextjs-mf');
+const { withFederatedSidecar } = require("@module-federation/nextjs-mf");
 
 module.exports = {
   reactStrictMode: true,
@@ -8,52 +8,5 @@ module.exports = {
     'snake-game',
     'tic-tac-toe',
     '2048-game',
-  ],
-
-  webpack(config, options) {
-    // const fileLoaderRule = config.module.rules.find((rule) =>
-    //   rule.test?.test?.('.svg')
-    // );
-
-    // config.module.rules.push(
-    //   {
-    //     ...fileLoaderRule,
-    //     test: /\.svg$/i,
-    //     resourceQuery: /url/,
-    //   },
-    //   {
-    //     test: /\.svg$/i,
-    //     issuer: /\.[jt]sx?$/,
-    //     resourceQuery: { not: /url/ },
-    //     use: ['@svgr/webpack'],
-    //   }
-    // );
-
-    // fileLoaderRule.exclude = /\.svg$/i;
-
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: 'portfolio',
-        filename: 'static/chunks/remoteEntry.js',
-        exposes: {
-          './welcome': './pages/index.tsx',
-          './2048': './pages/2048-game.tsx',
-          './snake': './pages/snake.tsx',
-          './tetris': './pages/tetris.tsx',
-          './tic-tac-toe': './pages/tick-tack-toe.tsx',
-        },
-        shared: {},
-        extraOptions: {
-          debug: true, // `false` by default
-          exposePages: true, // `false` by default
-          enableImageLoaderFix: false, // `false` by default
-          enableUrlLoaderFix: true, // `false` by default
-          skipSharingNextInternals: true, // `false` by default
-        },
-      })
-    );
-    config.output.publicPath = 'auto';
-
-    return config;
-  },
-};
+  ]
+}
